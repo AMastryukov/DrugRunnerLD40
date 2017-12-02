433ae79a0ca873f4dfd0f4bd4994bd5c7761cb20
MasterKey13 <Alexandermastryukov@gmail.com>
2017-12-02 14:45:13 -0500

    Player Movement
    Added game scene
    Implemented player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			rb2d.AddForce (new Vector2(0,18));
+		}
+
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+			rb2d.AddForce (new Vector2(0,-18));
+		}
+	}
+}
